f7a1f1e3f1f35ac89889359d85a4908397fc53b6
GreenFrog <Nymphaea@Pond.local>
2015-07-06 02:22:43 +0200

    Relocate mapping of DPAD from coolie hat to second analog stick

--- src_NormalMode/n64gc.c
+++ src_NormalMode/n64gc.c
@@ -1,35 +1,9 @@
 #include <avr/io.h>
-#include <avr/pgmspace.h>
 #include <util/delay.h>
 #include "report.h"
 #include "n64gc.h"
 
 //	PB0		-	data
-
-/*	UDLR
-	0000	-
-	0001	2
-	0010	6
-	0011	-
-	0100	4
-	0101	3
-	0110	5
-	0111	-
-	1000	0
-	1001	1
-	1010	7
-	1011	-
-	1100	-
-*/
-
-/*		0
-	7		1
-6				2
-	5		3
-		4
-*/
-
-const uchar n64gc_hat_lut[] PROGMEM  = { -1, 2, 6, -1, 4, 3, 5, -1, 0, 1, 7, -1, -1 };
 
 extern	uchar readnintendo(report_t *reportBuffer);
 
@@ -44,5 +18,20 @@ void ReadN64GC(report_t *reportBuffer)
 	DDRD	|= (1<<7);
 
 	dpad = readnintendo(reportBuffer);
-	reportBuffer->hat = pgm_read_byte(&n64gc_hat_lut[dpad]);
+
+	if (dpad & 0x8) {
+		reportBuffer->ry = -127;	// up
+	} else if (dpad & 0x4) {
+		reportBuffer->ry = 127;		// down
+	} else {
+		reportBuffer->ry = 0; 		// center
+	}
+
+	if (dpad & 0x1) {
+		reportBuffer->rx = 127;		// right
+	} else if (dpad & 0x2) {
+		reportBuffer->rx = -127;	// left
+	} else {
+		reportBuffer->rx = 0; 		// center
+	}
 }
